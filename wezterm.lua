e0ed33676d9ca1f8a1d69aaab2f0191b44c708f8
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-06-24 10:52:34 -0700

    [Wezterm] Don't warn about missing glyphs.

--- wezterm.lua
+++ wezterm.lua
@@ -13,6 +13,7 @@ end
 -- This is where you actually apply your config choices
 config.font = wezterm.font("Source Code Pro", {weight="Regular", stretch="Normal", style="Normal"}) -- /usr/share/fonts/adobe-source-code-pro/SourceCodePro-Regular.otf, FontConfig
 config.font_size = 8
+config.warn_about_missing_glyphs = false
 
 config.color_scheme = 'Solarized Dark (Gogh)'
 config.colors = {  -- patch Solarized Dark (Gogh) theme: make character under cursor visible
